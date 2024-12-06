69c48dafcdd90286c613661e7898b38fb0b9c751
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-12-06 16:11:22 +0100

    sample working

--- src/main/java/org/hiero/spring/ViewController.java
+++ src/main/java/org/hiero/spring/ViewController.java
@@ -1,12 +1,13 @@
 package org.hiero.spring;
 
+import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.TokenId;
 import com.openelements.hiero.base.HieroException;
 import com.openelements.hiero.base.NftClient;
+import com.openelements.hiero.base.data.Nft;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
@@ -14,9 +15,15 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Singleton
 @Path("/")
+@Produces(MediaType.TEXT_HTML)
 public class ViewController {
 
     private final Template index;
@@ -37,22 +44,36 @@ public class ViewController {
     }
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance index() {
-        return index.instance();
+    public TemplateInstance index() throws HieroException {
+        return index.data("nfts", getAllNftModels());
     }
 
     @POST
     @Path("/mint")
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance mint() {
-        return index.instance();
+    public TemplateInstance mint() throws HieroException {
+        String metadata = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now());
+        nftClient.mintNft(nftType, metadata.getBytes(StandardCharsets.UTF_8));
+        return index.data("nfts", getAllNftModels());
     }
 
     @POST
     @Path("/burnall")
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance burnall() {
-        return index.instance();
+    public TemplateInstance burnall() throws HieroException {
+        return index.data("nfts", getAllNftModels());
+    }
+
+    private Set<Nft> getAllNfts() throws HieroException {
+        //TODO: NftRepository implemenetation for Microprofile is missing
+        return Set.of();
+    }
+
+    private Set<NftModel> getAllNftModels() throws HieroException {
+        return getAllNfts().stream()
+                .map(nft -> new NftModel(nft.tokenId().toString(), Long.toString(nft.serial()),
+                        new String(nft.metadata(), StandardCharsets.UTF_8)))
+                .collect(Collectors.toSet());
+    }
+
+    public record NftModel(String name, String serial, String metadata) {
     }
 }
