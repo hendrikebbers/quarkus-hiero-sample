93ceb9013ce5316f1f6e4f7469529f47da4651c2
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-12-06 14:34:29 +0100

    hiero working

--- src/main/java/org/hiero/spring/ViewController.java
+++ src/main/java/org/hiero/spring/ViewController.java
@@ -7,25 +7,38 @@ import com.openelements.hiero.base.NftClient;
 import com.openelements.hiero.base.mirrornode.NftRepository;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/")
+@Singleton
 public class ViewController {
 
     @Inject
     private Template index;
 
     @Inject
-    private HieroContext hieroContext;
+    private NftClient nftClient;
+
+    private TokenId nftType;
+
+    @PostConstruct
+    public void init() {
+        try {
+            nftType = nftClient.createNftType("Quarkus-Sample", "QS");
+        } catch (HieroException e) {
+            throw new RuntimeException("Error in creating token", e);
+        }
+    }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance index() {
-        System.out.println(hieroContext.getOperatorAccount());
         return index.instance();
     }
 }
