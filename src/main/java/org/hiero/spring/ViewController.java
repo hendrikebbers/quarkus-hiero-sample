06624c5a3d9a8924297ab6898fc47c65c109a08f
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-12-06 15:59:53 +0100

    viewcontroller endpoints

--- src/main/java/org/hiero/spring/ViewController.java
+++ src/main/java/org/hiero/spring/ViewController.java
@@ -1,34 +1,34 @@
 package org.hiero.spring;
 
 import com.hedera.hashgraph.sdk.TokenId;
-import com.openelements.hiero.base.HieroContext;
 import com.openelements.hiero.base.HieroException;
 import com.openelements.hiero.base.NftClient;
-import com.openelements.hiero.base.mirrornode.NftRepository;
+import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/")
 @Singleton
+@Path("/")
 public class ViewController {
 
-    @Inject
-    private Template index;
+    private final Template index;
+
+    private final NftClient nftClient;
+
+    private final TokenId nftType;
 
     @Inject
-    private NftClient nftClient;
-
-    private TokenId nftType;
-
-    @PostConstruct
-    public void init() {
+    public ViewController(@Location("index") final Template index, final NftClient nftClient) {
+        this.index = index;
+        this.nftClient = nftClient;
         try {
             nftType = nftClient.createNftType("Quarkus-Sample", "QS");
         } catch (HieroException e) {
@@ -39,6 +39,20 @@ public class ViewController {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance index() {
+        return index.instance();
+    }
+
+    @POST
+    @Path("/mint")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance mint() {
+        return index.instance();
+    }
+
+    @POST
+    @Path("/burnall")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance burnall() {
         return index.instance();
     }
 }
